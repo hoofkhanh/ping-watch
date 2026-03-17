d227a2df5a748b8309db80abea05abbf08c580eb
Ho Tuan Khanh <hotuankhanh20112016@gmail.com>
2026-03-17 23:44:23 +0700

    Add handler for display argumentNotValidException

---
+++ src/main/java/com/hokhanh/ping_watch/handler/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.hokhanh.ping_watch.handler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
